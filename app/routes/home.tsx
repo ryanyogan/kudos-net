d264bfb9d5a8ff372ad48fcbda9caee44d52faff
Ryan Yogan <ryanyogan@gmail.com>
2023-02-03 14:55:29 -0600

    feat: always redirect to /home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,11 @@
 import type { LoaderArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
 import { requireUserId } from "~/utils/auth.server";
 
 export async function loader({ request }: LoaderArgs) {
   await requireUserId(request);
-  return redirect("/home");
+  return null;
+}
+
+export default function Home() {
+  return <div>Home</div>;
 }
